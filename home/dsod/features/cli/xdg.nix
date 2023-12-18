bfb6d31d6d236079f0175631ba2f31268923af27
dsod <daniel.soderling@gmail.com>
2023-12-18 18:28:34 +0100

    typescript fixes

--- home/dsod/features/cli/xdg.nix
+++ home/dsod/features/cli/xdg.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 let
   browser = [ "google-chrome.desktop" ];
-  documents = ["org.pwmt.zathura.desktop" ];
+  documents = [ "org.pwmt.zathura.desktop" ];
   imageviewer = [ "imv.desktop" ];
 in
 {
@@ -55,61 +55,64 @@ in
           "image/svg" = imageviewer;
           "image/svg+xml" = imageviewer;
         };
-        associations.added = {
-          "application/json" = browser;
-          "application/pdf" = documents;
+      associations.added = {
+        "application/json" = browser;
+        "application/pdf" = documents;
 
-          "text/html" = browser;
-          "text/xml" = browser;
-          "application/xml" = browser;
-          "application/xhtml+xml" = browser;
-          "application/xhtml_xml" = browser;
-          "application/rdf+xml" = browser;
-          "application/rss+xml" = browser;
-          "application/x-extension-htm" = browser;
-          "application/x-extension-html" = browser;
-          "application/x-extension-shtml" = browser;
-          "application/x-extension-xht" = browser;
-          "application/x-extension-xhtml" = browser;
+        "text/html" = browser;
+        "text/xml" = browser;
+        "application/xml" = browser;
+        "application/xhtml+xml" = browser;
+        "application/xhtml_xml" = browser;
+        "application/rdf+xml" = browser;
+        "application/rss+xml" = browser;
+        "application/x-extension-htm" = browser;
+        "application/x-extension-html" = browser;
+        "application/x-extension-shtml" = browser;
+        "application/x-extension-xht" = browser;
+        "application/x-extension-xhtml" = browser;
 
-          "x-scheme-handler/about" = browser;
-          "x-scheme-handler/ftp" = browser;
-          "x-scheme-handler/http" = browser;
-          "x-scheme-handler/https" = browser;
-          "x-scheme-handler/unknown" = browser;
+        "x-scheme-handler/about" = browser;
+        "x-scheme-handler/ftp" = browser;
+        "x-scheme-handler/http" = browser;
+        "x-scheme-handler/https" = browser;
+        "x-scheme-handler/unknown" = browser;
 
-          "x-scheme-handler/discord" = [ "discord.desktop" ];
-          "x-scheme-handler/slack" = [ "slack.desktop" ];
+        "x-scheme-handler/discord" = [ "discord.desktop" ];
+        "x-scheme-handler/slack" = [ "slack.desktop" ];
 
-          "audio/*" = [ "mpv.desktop" ];
-          "video/*" = [ "mpv.dekstop" ];
-          "image/jpg" = imageviewer;
-          "image/jpeg" = imageviewer;
-          "image/png" = imageviewer;
-          "image/tiff" = imageviewer;
-          "image/webp" = imageviewer;
-          "image/avif" = imageviewer;
-          "image/gif" = imageviewer;
-          "image/x-tiff" = imageviewer;
-          "image/svg" = imageviewer;
-          "image/svg+xml" = imageviewer;
-        };
+        "audio/*" = [ "mpv.desktop" ];
+        "video/*" = [ "mpv.dekstop" ];
+        "image/jpg" = imageviewer;
+        "image/jpeg" = imageviewer;
+        "image/png" = imageviewer;
+        "image/tiff" = imageviewer;
+        "image/webp" = imageviewer;
+        "image/avif" = imageviewer;
+        "image/gif" = imageviewer;
+        "image/x-tiff" = imageviewer;
+        "image/svg" = imageviewer;
+        "image/svg+xml" = imageviewer;
+      };
     };
 
     userDirs = {
       enable = true;
       createDirectories = true;
       extraConfig = {
-        XDG_DESKTOP_DIR="$HOME/desktop";
-        XDG_DOCUMENTS_DIR="$HOME/documents";
-        XDG_DOWNLOAD_DIR="$HOME/downloads";
-        XDG_MUSIC_DIR="$HOME/music";
-        XDG_PICTURES_DIR="$HOME/pictures";
-        XDG_PUBLICSHARE_DIR="$HOME/public";
-        XDG_TEMPLATES_DIR="$HOME/templates";
-        XDG_VIDEOS_DIR="$HOME/videos";
+        XDG_DESKTOP_DIR = "$HOME/desktop";
+        XDG_DOCUMENTS_DIR = "$HOME/documents";
+        XDG_DOWNLOAD_DIR = "$HOME/downloads";
+        XDG_MUSIC_DIR = "$HOME/music";
+        XDG_PICTURES_DIR = "$HOME/pictures";
+        XDG_PUBLICSHARE_DIR = "$HOME/public";
+        XDG_TEMPLATES_DIR = "$HOME/templates";
+        XDG_VIDEOS_DIR = "$HOME/videos";
         XDG_SCREENSHOTS_DIR = "$HOME/pictures/screenshots";
       };
     };
   };
+  home.file."documents/encrypted/.keep".text = "";
+  home.file."bruno/.keep".text = "";
+  home.file."obsidian/.keep".text = "";
 }
--- home/dsod/features/desktop/common/rclone/default.nix
+++ home/dsod/features/desktop/common/rclone/default.nix
@@ -7,6 +7,6 @@
     RCLONE_PASSWORD_COMMAND = "pass rclone/config/password";
   };
 
-  home.file."dotfiles/home/dsod/features/desktop/common/rclone/config.conf".source = config.lib.file.mkOutOfStoreSymlink /home/dsod/.config/rclone/rclone.conf;
+  xdg.configFile."rclone/rclone.conf".source = config.lib.file.mkOutOfStoreSymlink /home/dsod/dotfiles/home/dsod/features/desktop/common/rclone/config.conf;
 }
 
